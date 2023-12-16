40c9650ced36e1048f5d54856b128268ffe916e8
stella-mumbi <stella.mumbi@student.moringaschool.com>
2023-12-16 22:42:20 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution in this file!
+// Write your solution in this file!
+var customerName = "bob";
+
+
+function returnsCustomerName() {
+  return customerName;
+}
+
+  function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+  }
+
+  function setBestCustomer() {
+    bestCustomer = "not bob";
+  }
+
+  function overwriteBestCustomer() {
+    bestCustomer = "still not bob"; 
+  }
+
+  function overwriteBestCustomer() {
+    bestCustomer = "maybe bob"; 
+  }
+
+  const leastFavoriteCustomer = "Peter";
+
+  function changeLeastFavoriteCustomer() {
+
+    leastFavoriteCustomer = "Bob";
+  }
